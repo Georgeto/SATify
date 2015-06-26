2c56a4153fa6e68155462ddf5b4118d9a301caef
Georg <s0916144@mail.zih.tu-dresden.de>
2015-06-26 12:34:36 +0200

    interpret DICMAS as undirected graphs

--- satgen.py
+++ satgen.py
@@ -145,24 +145,25 @@ def satify_cnf(file_graph, ofile_cnf):
         # n1_2 -> (n2_3 | n5_3 | n6_3) = !n1_2 | n2_3 | n5_3 | n6_3
         #
         for n in g.nodes():
+            neighbors = g.neighbors(n)
             for p in xrange(1, node_count):
                 if p == 1: #Kreisbedingung
                     f.write("-" + node_alias(n, p, node_count) + " ")
-                    for predecessor in g.predecessors(n):
-                        f.write(node_alias(predecessor, node_count, node_count) + " ")
+                    for neighbor in neighbors:
+                        f.write(node_alias(neighbor, node_count, node_count) + " ")
                     end_cnf_line(f)
                     clause_count += 1
 
                 f.write("-" + node_alias(n, p, node_count) + " ")
-                for successor in g.successors(n):
-                    f.write(node_alias(successor, p+1, node_count) + " ")
+                for neighbor in neighbors:
+                    f.write(node_alias(neighbor, p+1, node_count) + " ")
                 end_cnf_line(f)
                 clause_count += 1
 
             if node_count > 1:
                 f.write("-" + node_alias(n, node_count, node_count) + " ")
-                for successor in g.successors(n):
-                    f.write(node_alias(successor, 1, node_count) + " ")
+                for neighbor in neighbors:
+                    f.write(node_alias(neighbor, 1, node_count) + " ")
                 end_cnf_line(f)
                 clause_count += 1
 
@@ -209,8 +210,7 @@ def minsat_cnf(file_cnf, ofile_res):
                 if literal[0] != '-' and literal != '0':
                     node_pos = from_node_alias(int(literal), node_count)
                     position[node_pos[0]] = node_pos[1]
-            print position
-            print tuple(x[0] for x in sorted(position.items(), key=operator.itemgetter(1)))
+            print ' '.join(map(str, list(x[0] for x in sorted(position.items(), key=operator.itemgetter(1)))))
             sys.exit(10)
 
         sys.exit(15)
